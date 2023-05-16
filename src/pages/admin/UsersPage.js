cad28526940029c4f997f8f2acb80a85a92e31b9
simonakancheva <s.kanchevaa@gmail.com>
2023-05-16 14:58:25 +0300

    add design in users

--- src/pages/admin/UsersPage.js
+++ src/pages/admin/UsersPage.js
@@ -3,6 +3,8 @@ import Sidebar from "../../components/Sidebar";
 import "./UsersPage.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../../actions/userActions";
+import { Card, Container, Button  } from "react-bootstrap";
+
 
 const UsersPage = () => {
     const usersReducer = useSelector((state) => state.usersReducer);
@@ -17,22 +19,41 @@ const UsersPage = () => {
         })
     },[]);
 
+    const handleUpdate = (userId) => {
+        // Обнови данните за потребителя
+        
+        console.log("Обновяване на потребител с идентификатор:", userId);
+      };
+
     return (
         <div className="quizzesPage__container">
         <div className="quizzesPage__sidebar">
             <Sidebar />
         </div>
         <div className="quizzesPage__content">
-            <h2>Потребители</h2>
-            {users.map(user => (
-                <div key={user.id}>
-                    <div>{user.firstName}</div>
-                    <div>{user.lastName}</div>
-                    <div>{(user.roles || []).map(r => r.roleName).join(", ")}</div>
-                    <div>{user.email}</div>
-                    <div>{user.enabled ? "АКТИВЕН" : "НЕАКТИВЕН"}</div>
-                </div>
-            ))}
+        <Container>
+          <h2>Потребители</h2>
+          {users.map((user) => (
+            <Card key={user.id} className="mb-3">
+              <Card.Body>
+                <Card.Title>Име: {user.firstName}</Card.Title>
+                <Card.Text>Фамилия: {user.lastName}</Card.Text>
+                <Card.Text>
+                 Роля: {(user.roles || []).map((r) => r.roleName).join(", ")}
+                </Card.Text>
+                <Card.Text>E-mail: {user.email}</Card.Text>
+                <Card.Text>
+                Статус: <span className={user.enabled ? "true" : "false"}>
+                    {user.enabled ? "АКТИВЕН" : "НЕАКТИВЕН"}
+                  </span>
+                </Card.Text>
+                <Button variant="primary" onClick={() => handleUpdate(user.id)}>
+                  Обнови
+                </Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </Container>
         </div>
         </div>
     );
